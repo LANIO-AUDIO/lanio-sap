a49203e837131f0f5242535b34d6a8a9aaa811da
Robin SHAMSNEJAD <rsham@free.fr>
2021-11-13 13:59:12 +0100

    Add commentary

--- armhf-toolchain.cmake
+++ armhf-toolchain.cmake
@@ -4,6 +4,7 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
 set(Qt5_DIR ${CMAKE_SYSROOT}/usr/lib/arm-linux-gnueabihf/cmake/Qt5)
 
+# Needed to skip compiler checks that would fail otherwise
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc)
