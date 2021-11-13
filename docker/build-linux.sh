a711c9b125323916c61f2e2ecd8e63abe7bc70c6
Robin SHAMSNEJAD <rsham@free.fr>
2021-11-13 12:44:43 +0100

    Split Linux builds in separate container

--- docker/build-linux.sh
+++ docker/build-linux.sh
@@ -2,8 +2,7 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-    $SCRIPT_DIR/build-mingw.sh \
- && $SCRIPT_DIR/build-armhf.sh \
+    $SCRIPT_DIR/build-armhf.sh \
  && $SCRIPT_DIR/build-amd64.sh \
  && exit 0 \
  || exit 1
